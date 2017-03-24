69843bf1ea24d12b826d0fabcd6b14b24b9cfe11
Alla Kraynova <aukra25@yandex.ru>
2017-03-24 11:45:16 +0300

    преподаватели

--- trainers.php
+++ trainers.php
@@ -13,132 +13,147 @@
 
   <?php include "blocks/page_header.php"; ?>
 
-  <div class="page-navigation">
-    <div class="container">
-      <ul class="breadcrumbs">
-        <li><a href="#">Главная</a></li>
-        <li><a href="#">Преподаватели</a></li>
-      </ul>
-    </div>
-  </div>
-  
-  <div class="page-section page-intro">
-    <div class="container">
-      <h1>Преподаватели</h1>
-      <p>
-        <i>Художественный руководитель студии — Садовский Владимир.</i> Все тренеры, преподающие в студии, являются профессиональными танцорами высокого уровня (класс А, S и M) 
-        по 10 обязательным танцам спортивно-бальной программы (европейская и латино-американская программы). Все обладают значительным опытом преподавания, в том числе парам «Про-Ам» 
-        (профессионал-любитель). Большинство тренеров владеют английским языком, некоторые имеют опыт работы и преподавания за рубежом.
-      </p>
-    </div>
-  </div>
-  
-  <section class="page-section trainers">
-    <div class="container">
-      <ul class="trainers__list">
-        <li class="trainers__item">
-          <figure>
-            <div class="trainers__img">
-              <img src="img/trainers/Sadovskiy@1x.jpg" alt="" srcset="img/trainers/Sadovskiy@1,5x.jpg 1.5x, img/trainers/Sadovskiy@2x.jpg 2x, img/trainers/Sadovskiy@3x.jpg 3x">
-            </div>
-            <figcaption class="trainers__title">
-              <p class="trainers__surname">Садовский</p>
-              <p class="trainers__name">Владимир Алексеевич</p>
-            </figcaption>
-          </figure>
-          <p>
-            Художественный руководитель студии — Садовский Владимир. Все тренеры, преподающие в студии, являются профессиональными танцорами высокого уровня 
-          </p>
-        </li>
-        <li class="trainers__item">
-          <figure>
-            <div class="trainers__img">
-              <img src="img/trainers/Gromov@1x.jpg" alt="" srcset="img/trainers/Gromov@1,5x.jpg 1.5x, img/trainers/Gromov@2x.jpg 2x, img/trainers/Gromov@3x.jpg 3x">
-            </div>
-            <figcaption class="trainers__title">
-              <p class="trainers__surname">Громов</p>
-              <p class="trainers__name">Никита Сергеевич</p>
-            </figcaption>
-          </figure>
-          <p>
-            Художественный руководитель студии — Садовский Владимир. Все тренеры, преподающие в студии, являются профессиональными танцорами высокого уровня 
-          </p>
-        </li>
-        <li class="trainers__item">
-          <figure>
-            <div class="trainers__img">
-              <img src="img/trainers/Sadovskaya@1x.jpg" alt="" srcset="img/trainers/Sadovskaya@1,5x.jpg 1.5x, img/trainers/Sadovskaya@2x.jpg 2x, img/trainers/Sadovskaya@3x.jpg 3x">
-            </div>
-            <figcaption class="trainers__title">
-              <p class="trainers__surname">Садовская</p>
-              <p class="trainers__name">Екатерина Дмитриевна</p>
-            </figcaption>
-          </figure>
-          <p>
-            Художественный руководитель студии — Садовский Владимир. Все тренеры, преподающие в студии, являются профессиональными танцорами высокого уровня 
-          </p>
-        </li>
-        <li class="trainers__item">
-          <figure>
-            <div class="trainers__img">
-              <img src="img/trainers/Usikova.jpg" alt="">
-            </div>
-            <figcaption class="trainers__title">
-              <p class="trainers__surname">Усикова</p>
-              <p class="trainers__name">Каролина Сергеевна</p>
-            </figcaption>
-          </figure>
-          <p>
-            Художественный руководитель студии — Садовский Владимир. Все тренеры, преподающие в студии, являются профессиональными танцорами высокого уровня 
-          </p>
-        </li>
-        <li class="trainers__item">
-          <figure>
-            <div class="trainers__img">
-              <img src="img/trainers/Yankovskiy@1x.jpg" alt="" srcset="img/trainers/Yankovskiy@1,5x.jpg 1.5x, img/trainers/Yankovskiy@2x.jpg 2x, img/trainers/Yankovskiy@3x.jpg 3x">
-            </div>
-            <figcaption class="trainers__title">
-              <p class="trainers__surname">Янковский</p>
-              <p class="trainers__name">Александр Анатольевич</p>
-            </figcaption>
-          </figure>
-          <p>
-            Художественный руководитель студии — Садовский Владимир. Все тренеры, преподающие в студии, являются профессиональными танцорами высокого уровня 
-          </p>
-        </li>
-      </ul>
-    </div>
-  </section>
-  
-  <section class="page-section">
-    <div class="container">
-      <div class="page-btns page-btns_horizontal">
-        <a href="dance_styles.php" class="btn btn_round btn_left">Танцевальные стили</a>
-        <a href="services.php" class="btn btn_round btn_left">Услуги</a>
-        <a href="shedule.php" class="btn btn_round btn_left">Расписание</a>
+  <main class="page page_nolead">
+    <div class="page-navigation">
+      <div class="page-navigation__container page-navigation__container_trainers container">
+        <ul class="breadcrumbs">
+          <li><a href="#">Главная</a></li>
+          <li><a href="#">Преподаватели</a></li>
+        </ul>
       </div>
-      <div class="mission mission_stylized updown-border">
-        <p class="misson__title">Наша цель &mdash;</p>
-        <p class="mission__text">
-          помочь Вам раскрыть свой потенциал, наш приоритет — индивидуальный подход к каждому. Ждем Вас в студии и желаем успешного путешествия в волшебный мир танца!
+    </div>
+    
+    <div class="page-section page-intro page-intro_trainers">
+      <div class="container">
+        <header class="page-title page-title_trainers">
+          <h1>Преподаватели</h1>
+        </header>
+        <p>
+          Художественный руководитель студии — Садовский Владимир.<br>
+          Все тренеры, преподающие в студии, являются профессиональными танцорами высокого уровня (класс А, S и M) 
+          по 10 обязательным танцам спортивно-бальной программы (европейская и латино-американская программы). Все обладают значительным опытом преподавания, в том числе парам «Про-Ам» 
+          (профессионал-любитель). Большинство тренеров владеют английским языком, некоторые имеют опыт работы и преподавания за рубежом.
         </p>
       </div>
-      <div class="сontact-info">
-        <div class="contact-info__phone">
-          <p>
-            Позвоните нам и запишитесь на занятия
-          </p>
-          <a href="tel:+74957784856">+7 (495) 778-4856</a>
-          <a href="tel:+79154007587">+7 (915) 400-7587</a>
+    </div>
+    
+    <section class="page-section trainers">
+      <div class="trainers__container container">
+        <ul class="trainers__list">
+          <li class="trainers__item">
+            <figure>
+              <div class="trainers__img">
+                <img src="img/trainers/Sadovskiy@1x.jpg" alt="" srcset="img/trainers/Sadovskiy@1,5x.jpg 1.5x, img/trainers/Sadovskiy@2x.jpg 2x, img/trainers/Sadovskiy@3x.jpg 3x">
+              </div>
+              <figcaption class="trainers__title">
+                <p class="trainers__surname">Садовский</p>
+                <p class="trainers__name">Владимир Алексеевич</p>
+              </figcaption>
+            </figure>
+            <div class="trainers__biography">
+              <p>
+                Художественный руководитель студии — Садовский Владимир. Все тренеры, преподающие в студии, являются профессиональными танцорами высокого уровня 
+              </p>
+            </div>
+          </li>
+          <li class="trainers__item">
+            <figure>
+              <div class="trainers__img">
+                <img src="img/trainers/Gromov@1x.jpg" alt="" srcset="img/trainers/Gromov@1,5x.jpg 1.5x, img/trainers/Gromov@2x.jpg 2x, img/trainers/Gromov@3x.jpg 3x">
+              </div>
+              <figcaption class="trainers__title">
+                <p class="trainers__surname">Громов</p>
+                <p class="trainers__name">Никита Сергеевич</p>
+              </figcaption>
+            </figure>
+            <div class="trainers__biography">
+              <p>
+                Художественный руководитель студии — Садовский Владимир. Все тренеры, преподающие в студии, являются профессиональными танцорами высокого уровня 
+              </p>
+            </div>
+          </li>
+          <li class="trainers__item">
+            <figure>
+              <div class="trainers__img">
+                <img src="img/trainers/Sadovskaya@1x.jpg" alt="" srcset="img/trainers/Sadovskaya@1,5x.jpg 1.5x, img/trainers/Sadovskaya@2x.jpg 2x, img/trainers/Sadovskaya@3x.jpg 3x">
+              </div>
+              <figcaption class="trainers__title">
+                <p class="trainers__surname">Садовская</p>
+                <p class="trainers__name">Екатерина Дмитриевна</p>
+              </figcaption>
+            </figure>
+            <div class="trainers__biography">
+              <p>
+                Художественный руководитель студии — Садовский Владимир. Все тренеры, преподающие в студии, являются профессиональными танцорами высокого уровня 
+              </p>
+            </div>
+          </li>
+          <li class="trainers__item">
+            <figure>
+              <div class="trainers__img">
+                <img src="img/trainers/Usikova.jpg" alt="">
+              </div>
+              <figcaption class="trainers__title">
+                <p class="trainers__surname">Усикова</p>
+                <p class="trainers__name">Каролина Сергеевна</p>
+              </figcaption>
+            </figure>
+            <div class="trainers__biography">
+              <p>
+                Художественный руководитель студии — Садовский Владимир. Все тренеры, преподающие в студии, являются профессиональными танцорами высокого уровня 
+              </p>
+            </div>
+          </li>
+          <li class="trainers__item">
+            <figure>
+              <div class="trainers__img">
+                <img src="img/trainers/Yankovskiy@1x.jpg" alt="" srcset="img/trainers/Yankovskiy@1,5x.jpg 1.5x, img/trainers/Yankovskiy@2x.jpg 2x, img/trainers/Yankovskiy@3x.jpg 3x">
+              </div>
+              <figcaption class="trainers__title">
+                <p class="trainers__surname">Янковский</p>
+                <p class="trainers__name">Александр Анатольевич</p>
+              </figcaption>
+            </figure>
+            <div class="trainers__biography">
+              <p>
+                Художественный руководитель студии — Садовский Владимир. Все тренеры, преподающие в студии, являются профессиональными танцорами высокого уровня 
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+    
+    <section class="page-section trainers-contacts">
+      <div class="trainers-contacts__container container">
+        <div class="page-btns trainers-contacts__btns">
+          <a href="dance_styles.php" class="trainers-contacts__btn btn btn_round btn_right btn_move-right">Танцевальные стили</a>
+          <a href="services.php" class="trainers-contacts__btn trainers-contacts__btn_services btn btn_round btn_right btn_move-right">Услуги</a>
+          <a href="shedule.php" class="trainers-contacts__btn btn btn_round btn_right btn_move-right">Расписание</a>
         </div>
-        <div class="contact-info__address">
-          <p>
-            Адрес студии: Летниковская, 6А, стр. 1
+        <div class="mission mission_stylized updown-border">
+          <p class="mission__title">Наша цель </p>
+          <p class="mission__text">
+            &mdash; помочь Вам раскрыть свой потенциал, наш приоритет — индивидуальный подход к каждому. Ждем Вас в студии и желаем успешного путешествия в волшебный мир танца!
           </p>
+        </div>
+        <div class="сontact-info contact-info_trainers">
+          <div class="contact-info__phone">
+            <p>
+              Позвоните нам и запишитесь на занятия
+            </p>
+            <a href="tel:+74957784856">+7 (495) 778-4856</a>
+            <a href="tel:+79154007587">+7 (915) 400-7587</a>
+          </div>
+          <div class="contact-info__address">
+            <p>
+              Адрес студии: Летниковская, 6А, стр. 1
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </main>
   
   
 
