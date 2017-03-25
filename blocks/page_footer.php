53658017705287aefe0792b6dd44d48ae7a40278
Alla Kraynova <aukra25@yandex.ru>
2017-03-26 00:15:12 +0300

    svg charisma

--- blocks/page_footer.php
+++ blocks/page_footer.php
@@ -15,7 +15,7 @@
       </ul>
       <a href="#" class="page-footer__developer">
         CHARISMA 
-        <img src="img/charisma.png" alt="" width="23" height="15"> 
+        <svg width="23" height="15" version="1.1" viewBox="0 0 117.75453 78.494797" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"><g transform="matrix(1.3333 0 0 -1.3333 0 78.495)"><path class="cross" d="m42.232 40.476h3.851v-22.078h-3.851z" fill="#fff"/><path class="cross" d="m33.115 31.36h22.085v-3.849h-22.085z" fill="#fff"/><g transform="translate(51.518 58.871)"><path d="m0 0v-7.691h29.102v-14.385h7.696v22.076z" fill="#fff"/></g><g transform="translate(0 22.076)"><path d="m0 0v-22.076h36.796v7.692h-29.099v14.384z" fill="#fff"/></g><g transform="translate(.0018 58.871)"><path d="m0 0v-22.076h7.696v14.385h29.099v7.691z" fill="#fff"/></g><g transform="translate(80.62 22.076)"><path d="m0 0v-14.384h-29.102v-7.692h36.798v22.076z" fill="#fff"/></g></g></svg> 
       </a>
     </div>
   </div>
