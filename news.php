9d63ae9b44705c7f53e8b3d9f7ecf2c2a5ac654f
Alla Kraynova <aukra25@yandex.ru>
2017-03-25 17:51:40 +0300

    Новости

--- news.php
+++ news.php
@@ -13,155 +13,187 @@
 
   <?php include "blocks/page_header.php"; ?>
 
-  <div class="page-navigation">
-    <div class="container">
-      <ul class="breadcrumbs">
-        <li><a href="#">Главная</a></li>
-        <li><a href="#">Новости</a></li>
-      </ul>
+  <main class="page page_nolead">
+    <div class="page-navigation">
+      <div class="page-navigation__container page-navigation__container_news container">
+        <ul class="breadcrumbs">
+          <li><a href="#">Главная</a></li>
+          <li><a>Новости</a></li>
+        </ul>
+      </div>
     </div>
-  </div>
-  
-  <section class="page-section news">
-    <div class="container">
-      <ul class="news__tabs">
-        <li class="active"><a href="#tab1">Статьи</a></li>
-        <li><a href="#tab2">События</a></li>
-        <li><a href="#tab3">Фотографии</a></li>
-      </ul>
-      
-      <div class="news__tabs-content">
-        <div id="tab1" class="news__tab-pane active">
-          <h2>Статьи</h2>
-          <article class="news__item">
-            <header class="news__title">
-              <h3>
-                <a href="#">У нашей звёздной Каролины бронза</a>
-              </h3>
-              <time datetime="2016-12-25">25.12.2016</time>
-            </header>
-            <a href="#"  class="news__img">
-               <picture>
-                 <img src="img/news/news1.jpg" alt="">
-               </picture>
-            </a>
-            <p>
-              Каролина Усикова - тренер танцевальной студии "DANCE OLYMP" - заняла 3 место
-            </p>
-          </article>
-          <article class="news__item">
-            <header class="news__title">
-              <h3>
-                <a href="">Успешное выступление нашей пары на Impire dance championship.</a>
-              </h3>
-              <time datetime="2016-12-18">18.12.2016</time>
-            </header>
-            <a href="#"  class="news__img">
-               <picture>
-                 <img src="" alt="">
-               </picture>
-            </a>
-            <p>
-              Каролина Усикова - тренер танцевальной студии "DANCE OLYMP" - заняла 3 место
-            </p>
-          </article>
-          <article class="news__item">
-            <header class="news__title">
-              <h3>
-                <a href="">Успешное выступление нашей пары на Impire dance championship.</a>
-              </h3>
-              <time datetime="2016-12-18">18.12.2016</time>
-            </header>
-            <a href="#"  class="news__img">
-               <picture>
-                 <img src="" alt="">
-               </picture>
-            </a>
-            <p>
-              Каролина Усикова - тренер танцевальной студии "DANCE OLYMP" - заняла 3 место
-            </p>
-          </article>
-          <article class="news__item">
-            <header class="news__title">
-              <h3>
-                <a href="">Успешное выступление нашей пары на Impire dance championship.</a>
-              </h3>
-              <time datetime="2016-12-18">18.12.2016</time>
-            </header>
-            <a href="#"  class="news__img">
-               <picture>
-                 <img src="" alt="">
-               </picture>
-            </a>
-            <p>
-              Каролина Усикова - тренер танцевальной студии "DANCE OLYMP" - заняла 3 место
-            </p>
-          </article>
-          <div class="news__tab-navigation">
-            <a href="#" class="btn btn__arrow-left">В начало</a>
-            <ul class="paginator">
-              <li><a href="#">Назад</a></li>
-              <li><a href="#">3</a></li>
-              <li><a href="#">4</a></li>
-              <li><a href="#">5</a></li>
-              <li><a href="#">6</a></li>
-              <li><a href="#">7</a></li>
-              <li><a href="#">8</a></li>
-              <li><a href="3">Вперед</a></li>
+    
+    <section class="page-section news">
+      <div class="news__container container">
+        <ul class="news__tabs updown-border">
+          <li class="news__tabs-item active"><a href="#tab1">Статьи</a></li>
+          <li class="news__tabs-item"><a href="#tab2">События</a></li>
+          <li class="news__tabs-item"><a href="#tab3">Фотографии</a></li>
+        </ul>
+        
+        <div class="news__tabs-content">
+          <div id="tab1" class="news__tab-pane active">
+            <h2>Статьи</h2>
+            <div class="news__tab-content">
+              <article class="news__item">
+                <a href="#"  class="news__img">
+                  <img src="img/news/news1.jpg" alt="">
+                </a>
+                <div class="news__content">
+                  <header class="news__title">
+                    <h3>
+                      <a href="#">У нашей звёздной Каролины бронза</a>
+                    </h3>
+                    <time datetime="2016-12-25">25.12.2016</time>
+                  </header>
+                  <p>
+                    Каролина Усикова - тренер танцевальной студии "DANCE OLYMP" - заняла 3 место
+                  </p>
+                </div>
+              </article>
+              <article class="news__item">
+                <a href="#"  class="news__img">
+                  <img src="" alt="">
+                </a>
+                <div class="news__content">
+                  <header class="news__title">
+                    <h3>
+                      <a href="">Успешное выступление нашей пары на Impire dance championship.</a>
+                    </h3>
+                    <time datetime="2016-12-18">18.12.2016</time>
+                  </header>
+                  <p>
+                    Каролина Усикова - тренер танцевальной студии "DANCE OLYMP" - заняла 3 место
+                  </p>
+                </div>
+              </article>
+              <article class="news__item">
+                <a href="#"  class="news__img">
+                  <img src="" alt="">
+                </a>
+                <div class="news__content">
+                  <header class="news__title">
+                    <h3>
+                      <a href="">Успешное выступление нашей пары на Impire dance championship.</a>
+                    </h3>
+                    <time datetime="2016-12-18">18.12.2016</time>
+                  </header>
+                  <p>
+                    Каролина Усикова - тренер танцевальной студии "DANCE OLYMP" - заняла 3 место
+                  </p>
+                </div>
+              </article>
+              <article class="news__item">
+                <a href="#"  class="news__img">
+                  <img src="" alt="">
+                </a>
+                <div class="news__content">
+                  <header class="news__title">
+                    <h3>
+                      <a href="">Успешное выступление нашей пары на Impire dance championship.</a>
+                    </h3>
+                    <time datetime="2016-12-18">18.12.2016</time>
+                  </header>
+                  <p>
+                    Каролина Усикова - тренер танцевальной студии "DANCE OLYMP" - заняла 3 место
+                  </p>
+                </div>
+              </article>
+            </div>
+            <div class="news__tab-navigation">
+              <a href="#" class="news__btn btn btn_arrow-left">В начало</a>
+              <ul class="paginator">
+                <li class="paginator__item"><a href="#" class="btn btn_round btn_empty btn_left btn_move-left"></a></li>
+                <li class="paginator__item"><a href="#">3</a></li>
+                <li class="paginator__item"><a href="#">4</a></li>
+                <li class="paginator__item active"><a>5</a></li>
+                <li class="paginator__item"><a href="#">6</a></li>
+                <li class="paginator__item"><a href="#">7</a></li>
+                <li class="paginator__item"><a href="#">8</a></li>
+                <li class="paginator__item"><a href="#" class="btn btn_round btn_empty btn_right btn_move-right"></a></li>
+              </ul>
+            </div>
+          </div>
+          <div id="tab2" class="news__tab-pane fade">
+            <h2>События</h2>
+            <ul class="news__events">
+              <li class="news__event-item">
+                <div class="news__title">
+                  <a href="">Открыты новые группы</a>
+                  <time datetime="2016-07-23">23.07.2016</time>
+                </div>
+              </li>
+              <li class="news__event-item">
+                <div class="news__title">
+                  <a href="">Подарочные сертификаты</a>
+                  <time datetime="2015-12-23">23.12.2015</time>
+                </div>
+              </li>
+              <li class="news__event-item">
+                <div class="news__title">
+                  <a href="">Танцы на корпоративе</a>
+                  <time datetime="2015-12-23">23.12.2015</time>
+                </div>
+              </li>
+              <li class="news__event-item">
+                <div class="news__title">
+                  <a href="">Успешное выступление нашей пары на Impire dance championship</a>
+                  <time datetime="2015-12-23">23.12.2015</time>
+                </div>
+              </li>
+              <li class="news__event-item">
+                <div class="news__title">
+                  <a href="">Финал проекта "Танцы со Звездами"</a>
+                  <time datetime="2015-12-23">23.12.2015</time>
+                </div>
+              </li>
+            </ul>
+            <div class="news__tab-navigation">
+              <a href="#" class="news__btn btn btn_arrow-left">В начало</a>
+              <ul class="paginator">
+                <li class="paginator__item"><a href="#" class="btn btn_round btn_empty btn_left btn_move-left"></a></li>
+                <li class="paginator__item"><a href="#">3</a></li>
+                <li class="paginator__item"><a href="#">4</a></li>
+                <li class="paginator__item active"><a>5</a></li>
+                <li class="paginator__item"><a href="#">6</a></li>
+                <li class="paginator__item"><a href="#">7</a></li>
+                <li class="paginator__item"><a href="#">8</a></li>
+                <li class="paginator__item"><a href="#" class="btn btn_round btn_empty btn_right btn_move-right"></a></li>
+              </ul>
+            </div>        
+          </div>
+          <div id="tab3" class="news__tab-pane fade">
+            <h2>Фотографии</h2>
+            <div class="news__gallery-img"><img src="" alt=""></div>
+            <ul class="news__gallery-controls">
+              <li class="news__gallery-btn"><a href="#" class="btn btn_round btn_empty btn_left btn_move-left"></a></li>
+              <li class="news__gallery-item">
+                <a href="#">
+                  <img src="" alt="">
+                </a>
+              </li>
+              <li  class="news__gallery-item">
+                <a href="#">
+                  <img src="" alt="">
+                </a>
+              </li>
+              <li class="news__gallery-item">
+                <a href="#">
+                  <img src="" alt="">
+                </a>
+              </li>
+              <li class="news__gallery-item">
+                <a href="#">
+                  <img src="" alt="">
+                </a>
+              </li>
+              <li class="news__gallery-btn"><a href="#" class="btn btn_round btn_empty btn_right btn_move-right"></a></li>
             </ul>
           </div>
         </div>
-        <div id="tab2" class="news__tab-pane fade">
-          <h3>События</h3>
-          <article class="news__item">
-            <header class="news__header">
-              <h3><a href="#"></a></h3>
-              <time></time>
-            </header>
-            <p></p>
-          </article>
-          <article class="news__item">
-            <header class="news__header">
-              <h3><a href="#"></a></h3>
-              <time></time>
-            </header>
-            <p></p>
-          </article>
-          <article class="news__item">
-            <header class="news__header">
-              <h3><a href="#"></a></h3>
-              <time></time>
-            </header>
-            <p></p>
-          </article>
-          <article class="news__item">
-            <header class="news__header">
-              <h3><a href="#"></a></h3>
-              <time></time>
-            </header>
-            <p></p>
-          </article>          
-        </div>
-        <div id="tab3" class="news__tab-pane fade">
-          <h3>Фотографии</h3>
-          <ul class="news__gallery">
-            <li><a href="#">
-                <picture><img src="" alt=""></picture>
-              </a></li>
-            <li><a href="#">
-                <picture><img src="" alt=""></picture>
-              </a></li>
-            <li><a href="#">
-                <picture><img src="" alt=""></picture>
-              </a></li>
-            <li><a href="#">
-                <picture><img src="" alt=""></picture>
-              </a></li>
-          </ul>
-        </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </main>
 
   <?php include "blocks/page_footer.php"; ?>
 
